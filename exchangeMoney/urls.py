40b64b088ec7d2d8c20dfb2287d8f1d24d883d62
Yeco Master <oscco.yoshin@gmail.com>
2019-05-15 01:49:10 -0500

    settings static url config in URL

--- exchangeMoney/urls.py
+++ exchangeMoney/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from exchange.views import IndexView
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
    	path('',IndexView.as_view(), name='home'),
-]
+]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
